4fe03644bfab383590f573e76043d5f3dc441741
brianlau <blau70@my.bcit.ca>
2026-01-30 13:14:19 -0800

    IDevice finished added the other devices

--- comp_2522_lab03/src/code/ca/bcit/comp2522/appledevices/IDevice.java
+++ comp_2522_lab03/src/code/ca/bcit/comp2522/appledevices/IDevice.java
@@ -1,10 +1,39 @@
 package ca.bcit.comp2522.appledevices;
 
-abstract public class IDevice
+/**
+ * Abstract class for all IDevices (IPhone, IPad, IPod).
+ *
+ * @author Brian Lau
+ * @author Sebastian Roby
+ * @author Samien Munwar
+ * @version 1.0
+ */
+public abstract class IDevice
 {
+    private final String purpose;
 
+    /**
+     * Constructor for IDevice
+     *
+     * @param purpose String
+     */
+    public IDevice(final String purpose)
+    {
+        this.purpose = purpose;
+    }
 
+    /**
+     * Returns purpose.
+     *
+     * @return purpose
+     */
+    public final String getPurpose()
+    {
+        return purpose;
+    }
 
-
-
+    /**
+     * Forces child class's to have a printDetails method.
+     */
+    public abstract void printDetails();
 }
